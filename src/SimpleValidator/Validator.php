6cb3292c5ce61b99aecc800ea43895f8f98136ba
Jack Kwakman <zwartejack@gmail.com>
2017-01-23 11:22:22 +0100

    Removed php @ warning suppression
    Anonymous function handling was causing massive warnings in our API logs:
    Severity: Warning  --> get_class() expects parameter 1 to be object, string given 
    
    get_class can only be used on objects so it is quite safe to first check if we have an object. Otherwise E_WARNINGS are thrown.

--- src/SimpleValidator/Validator.php
+++ src/SimpleValidator/Validator.php
@@ -231,7 +231,7 @@ class Validator {
                     /**
                      * Handle anonymous functions
                      */
-                    if (@get_class($closure) == 'Closure') {
+                    if(is_object($example) && get_class($example) == 'Closure') {
                         $refl_func = new \ReflectionFunction($closure);
                         $validation = $refl_func->invokeArgs($params);
                     }/**
